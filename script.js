e4cd0d7d20ce701a73887f106a0feefd5669cac2
Joschyama <109805410+Joschyama@users.noreply.github.com>
2024-01-27 22:57:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let randomElement;
+let scoreid = document.getElementById('score');
+let scoretextid = document.getElementById('scoretext')
+let scorenum = 0;
+function randomImage() {
+    fetch('database.json')
+        .then(response => response.json())
+        .then(data => {
+            const randomIndex = Math.floor(Math.random() * data.length);
+            randomElement = data[randomIndex];
+            console.log("Random Element:", randomElement);
+            const randomElementParagraph = document.getElementById('image');
+            randomElementParagraph.src = `${randomElement.Link}`;
+            console.log(randomElement.Link);
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+}
+
+randomImage();
+
+function LouLoubutton() {
+    if (randomElement.Katze.includes("LouLou")) {
+        score.innerHTML = scorenum +=1;
+        scoretext.innerHTML = "Das war Richtig, das war LouLou!";
+        randomImage();
+    } else {
+        scoretext.innerHTML = "Das war Falsch, das war Cocolino!";
+        randomImage();
+    }
+}
+function Cocobutton() {
+    if (randomElement.Katze.includes("Cocolino")) {
+        score.innerHTML = scorenum +=1;
+        scoretext.innerHTML = "Das war Richtig, das war Cocolino!";
+        randomImage();
+    } else {
+        scoretext.innerHTML = "Das war Falsch, das war LouLou!";
+        randomImage();
+    }
+}
